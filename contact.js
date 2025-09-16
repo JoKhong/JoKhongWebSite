db7d3667d2617e24e2bcb787033004f33bebe373
JoKhong <JoKhong@users.noreply.github.com>
2025-09-16 12:18:41 +0800

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,11 @@
+
+function AttachToDownload()
+{
+    $("#resume").click( function() {
+       
+        window.open("./files/KhongMunWei_CV_150925.pdf", "_blank")
+
+    });
+}
+
+AttachToDownload();
